71377058b20f775557556dcda928e14014b5c45c
akimoto <ec2-user@ip-172-31-14-63.us-east-2.compute.internal>
2018-04-27 07:47:33 +0000

    want

--- app/Http/Controllers/ItemsController.php.php
+++ app/Http/Controllers/ItemsController.php.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \App\Item;
 
-class ItemsController.php extends Controller
+class ItemsController extends Controller
 {
     public function create()
     {
@@ -37,4 +37,16 @@ class ItemsController.php extends Controller
             'items' => $items,
         ]);
     }
+    
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
+    }
+    
 }
